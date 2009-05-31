2274531866e0633c1d7c392539bed0a776b088ad
OtstavnovMaxim <OtstavnovMaxim@317a9804-357f-11de-a81c-fde7d73ceb89>
2009-05-31 19:10:44 +0000

    Добавлены кнопки "Refresh", которые обновляют списки постов и списки комментариев соответственно

--- trunk/AndroBlogger/src/com/sadko/androblogger/ViewBlog.java
+++ trunk/AndroBlogger/src/com/sadko/androblogger/ViewBlog.java
@@ -1,5 +1,6 @@
 package com.sadko.androblogger;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,35 +9,81 @@ import java.util.Map;
 import com.google.gdata.data.Entry;
 import com.google.gdata.data.Feed;
 import com.google.gdata.data.TextContent;
+import com.google.gdata.util.ServiceException;
+import com.sadko.androblogger.db.DBAdapter;
 import com.sadko.androblogger.util.Alert;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
 
 public class ViewBlog extends ListActivity {
 	private final String TAG = "ViewBlog";
 	public static Feed resultFeed = null;
 	public static Entry currentEntry = null;
-	private int selectedItemId = -1;
+	private ProgressDialog viewProgress = null;
+	private final String MSG_KEY = "value";
+	private DBAdapter mDbHelper;
+	private static Cursor setting = null;
 	int viewStatus = 0;
+	private int attempt = 0;
+
+	final Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			Bundle content = msg.getData();
+			String progressId = content.getString(MSG_KEY);
+			if (progressId != null) {
+				if (progressId.equals("1")) {
+					viewProgress.setMessage("Preparing blog config...");
+				} else if (progressId.equals("2")) {
+					viewProgress.setMessage("Authenticating...");
+				} else if (progressId.equals("3")) {
+					viewProgress.setMessage("Receiving blog entries...");
+				} else if (progressId.equals("4")) {
+					viewProgress.setMessage("Done...");
+				}
+			}
+		}
+	};
+
+	final Runnable mViewResults = new Runnable() {
+		public void run() {
+			showViewStatus();
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.viewblog);
+		mDbHelper = new DBAdapter(this);
+		try {
+			mDbHelper.open();
+		} catch (SQLException e) {
+			Log.e(TAG, "Database has not opened");
+		}
+		setting = mDbHelper.fetchSettindById(1);
+		startManagingCursor(setting);
 
 		resultFeed = MainActivity.resultFeed;
 		Log.i(TAG, "ResultFeed obtained from MainActivity");
@@ -44,7 +91,7 @@ public class ViewBlog extends ListActivity {
 		int w = this.getWindow().getWindowManager().getDefaultDisplay()
 				.getWidth() - 12;
 		((Button) this.findViewById(R.id.BackToMainActivity)).setWidth(w / 2);
-		((Button) this.findViewById(R.id.Details)).setWidth(w / 2);
+		((Button) this.findViewById(R.id.RefreshPostsList)).setWidth(w / 2);
 
 		TextView blogTitle = (TextView) ViewBlog.this
 				.findViewById(R.id.BlogTitle);
@@ -54,10 +101,12 @@ public class ViewBlog extends ListActivity {
 		Map<String, Object> data;
 		int maxCharTitle = 22;
 		int maxCharContent = 30;
-		if(this.getWindow().getWindowManager().getDefaultDisplay().getOrientation()==ActivityInfo.SCREEN_ORIENTATION_PORTRAIT){
+		if (this.getWindow().getWindowManager().getDefaultDisplay()
+				.getOrientation() == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
 			maxCharTitle = 40;
 			maxCharContent = 50;
-		}else if(this.getWindow().getWindowManager().getDefaultDisplay().getOrientation()==ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+		} else if (this.getWindow().getWindowManager().getDefaultDisplay()
+				.getOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
 			maxCharTitle = 22;
 			maxCharContent = 30;
 		}
@@ -66,35 +115,43 @@ public class ViewBlog extends ListActivity {
 			Entry entry = resultFeed.getEntries().get(j);
 			try {
 				String truncatedTitle = null;
-				if(entry.getTitle().getPlainText().length()>maxCharTitle){
-					truncatedTitle = entry.getTitle().getPlainText().substring(0, maxCharTitle)+"...";
-				} else{
+				if (entry.getTitle().getPlainText().length() > maxCharTitle) {
+					truncatedTitle = entry.getTitle().getPlainText().substring(
+							0, maxCharTitle)
+							+ "...";
+				} else {
 					truncatedTitle = entry.getTitle().getPlainText();
 				}
 				data.put("line1", truncatedTitle);
 				String truncatedContent = null;
-				if(((TextContent) entry.getContent()).getContent().getPlainText().length()>maxCharContent){
-					truncatedContent = ((TextContent) entry.getContent()).getContent().getPlainText().substring(0, maxCharContent)+"...";
-				} else{
-					truncatedContent = ((TextContent) entry.getContent()).getContent().getPlainText();
+				if (((TextContent) entry.getContent()).getContent()
+						.getPlainText().length() > maxCharContent) {
+					truncatedContent = ((TextContent) entry.getContent())
+							.getContent().getPlainText().substring(0,
+									maxCharContent)
+							+ "...";
+				} else {
+					truncatedContent = ((TextContent) entry.getContent())
+							.getContent().getPlainText();
 				}
-				
+
 				data.put("line2", truncatedContent);
-				
-				data.put("line3", entry.getUpdated().toStringRfc822()
-						.substring(
-								0,
-								entry.getUpdated().toStringRfc822()
-										.length() - 5));
+
+				data.put("line3",
+						entry.getUpdated().toStringRfc822()
+								.substring(
+										0,
+										entry.getUpdated().toStringRfc822()
+												.length() - 5));
 				resourceNames.add(data);
 			} catch (Resources.NotFoundException nfe) {
-				Log.e(TAG, "NotFoundException "+nfe.getMessage());
+				Log.e(TAG, "NotFoundException " + nfe.getMessage());
 			}
 		}
 
 		SimpleAdapter notes = new SimpleAdapter(this, resourceNames,
-				R.layout.row, new String[]{"line1", "line2", "line3"}, new int[]{
-						R.id.text1, R.id.text2, R.id.text3});
+				R.layout.row, new String[]{"line1", "line2", "line3"},
+				new int[]{R.id.text1, R.id.text2, R.id.text3});
 		setListAdapter(notes);
 
 		this.findViewById(R.id.BackToMainActivity).setOnClickListener(
@@ -106,38 +163,173 @@ public class ViewBlog extends ListActivity {
 						finish();
 					}
 				});
-		ListView currentListView = getListView();
-		currentListView.setOnItemSelectedListener(new OnItemSelectedListener() {
-			@SuppressWarnings("unchecked")
-			@Override
-			public void onItemSelected(AdapterView parent, View v,
-					int position, long id) {
-				Log.d(TAG, "Selected: " + id + " element");
-				selectedItemId = (int) id;
-			}
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public void onNothingSelected(AdapterView parent) {
-				Log.d(TAG, "Selected: -1 element");
-			}
-		});
-		this.findViewById(R.id.Details).setOnClickListener(
+		this.findViewById(R.id.RefreshPostsList).setOnClickListener(
 				new OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						if (selectedItemId >= 0) {
-							currentEntry = resultFeed.getEntries().get(
-									selectedItemId);
-							Intent i = new Intent(ViewBlog.this, ViewPost.class);
-							startActivity(i);
-							finish();
-						} else {
-							Alert.showAlert(ViewBlog.this, "Nothing selected",
-									"Please select some post");
+						viewBlogPosts();
+					}
+
+				});
+		// ListView currentListView = getListView();
+		/*
+		 * currentListView.setOnItemSelectedListener(new
+		 * OnItemSelectedListener() {
+		 * 
+		 * @SuppressWarnings("unchecked")
+		 * 
+		 * @Override public void onItemSelected(AdapterView parent, View v, int
+		 * position, long id) { Log.d(TAG, "Selected: " + id + " element");
+		 * selectedItemId = (int) id; }
+		 * 
+		 * @SuppressWarnings("unchecked")
+		 * 
+		 * @Override public void onNothingSelected(AdapterView parent) {
+		 * Log.d(TAG, "Selected: -1 element"); } });
+		 */
+		/*
+		 * this.findViewById(R.id.Details).setOnClickListener( new
+		 * OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { if (selectedItemId >= 0) {
+		 * currentEntry = resultFeed.getEntries().get( selectedItemId); Intent i
+		 * = new Intent(ViewBlog.this, ViewPost.class); startActivity(i);
+		 * finish(); } else { Alert.showAlert(ViewBlog.this, "Nothing selected",
+		 * "Please select some post"); } } });
+		 */
+	}
+
+	private void showViewStatus() {
+		viewProgress.dismiss();
+		if (viewStatus != 5) {
+			/*
+			 * Alert.showAlert(this, "Viewing failed", "Error code " +
+			 * viewStatus + "\nTry again.");
+			 */
+			Alert.showAlert(this, "Viewing failed", "Error code " + viewStatus,
+					"Try again", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+							viewBlogPosts();
+						}
+					}, "Cancel", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.cancel();
+						}
+					});
+		}
+	}
+
+	protected void viewBlogPosts() {
+		viewProgress = ProgressDialog.show(ViewBlog.this,
+				"Viewing blog entries", "Starting to view blog entries...");
+
+		Thread viewThread = new Thread() {
+			public void run() {
+				Bundle status = new Bundle();
+				mHandler.getLooper();
+				Looper.prepare();
+				Message statusMsg = mHandler.obtainMessage();
+				viewStatus = 0;
+				status.putString(MSG_KEY, "1");
+				statusMsg.setData(status);
+				mHandler.sendMessage(statusMsg);
+				boolean viewOk = false;
+				String username = setting.getString(setting
+						.getColumnIndexOrThrow(DBAdapter.KEY_LOGIN));
+				String password = setting.getString(setting
+						.getColumnIndexOrThrow(DBAdapter.KEY_PASSWORD));
+				BlogInterface blogapi = null;
+				BlogConfigBLOGGER.BlogInterfaceType typeEnum = BlogConfigBLOGGER
+						.getInterfaceTypeByNumber(1);
+				blogapi = BlogInterfaceFactory.getInstance(typeEnum);
+				Log.d(TAG, "Using interface type: " + typeEnum);
+				CharSequence postconfig = "";
+				blogapi.setInstanceConfig(postconfig);
+				status.putString(MSG_KEY, "2");
+				statusMsg = mHandler.obtainMessage();
+				statusMsg.setData(status);
+				mHandler.sendMessage(statusMsg);
+				String auth_id = null;
+				boolean authFlag = false;
+				attempt = 0;
+				while ((attempt <= MainActivity.AMOUNTOFATTEMPTS)
+						&& (!authFlag)) {
+					try {
+						auth_id = blogapi.getAuthId(username, password);
+						authFlag = true;
+						attempt = 0;
+					} catch (com.google.gdata.util.AuthenticationException e) {
+						Log.e(TAG, "AuthenticationException " + e.getMessage());
+						attempt++;
+					}
+				}
+				viewStatus = 1;
+				Log.d(TAG, "Got auth token:" + auth_id);
+				viewStatus = 2;
+				if (auth_id != null) {
+					status.putString(MSG_KEY, "3");
+					statusMsg = mHandler.obtainMessage();
+					statusMsg.setData(status);
+					mHandler.sendMessage(statusMsg);
+					authFlag = false;
+					attempt = 0;
+					while ((attempt <= MainActivity.AMOUNTOFATTEMPTS)
+							&& (!authFlag)) {
+						try {
+							MainActivity.resultFeed = blogapi
+									.getAllPosts(username, password);
+							Log.i(TAG, "Blog entries successfully received");
+							viewOk = true;
+							authFlag = true;
+							attempt = 0;
+						} catch (ServiceException e) {
+							attempt++;
+							Log
+									.e(TAG,
+											"ServiceException (getAllPosts(username, password))");
+						} catch (IOException e) {
+							attempt++;
+							Log
+									.e(TAG,
+											"Exception (getAllPosts(username, password))");
 						}
 					}
-				});
+				} else {
+					viewStatus = 3;
+				}
+				status.putString(MSG_KEY, "4");
+				statusMsg = mHandler.obtainMessage();
+				statusMsg.setData(status);
+				mHandler.sendMessage(statusMsg);
+				if (viewOk) {
+					Log.d(TAG, "Success!");
+					viewStatus = 5;
+				} else {
+					Log.d(TAG, "Viewing of the blog failed!");
+					viewStatus = 4;
+				}
+				mHandler.post(mViewResults);
+
+				if ((resultFeed != null) && (viewOk)) {
+					Intent i = new Intent(ViewBlog.this, ViewBlog.class);
+					startActivity(i);
+					finish();
+				}
+			}
+		};
+		ConnectivityManager cm = (ConnectivityManager) ViewBlog.this
+				.getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo netinfo = cm.getActiveNetworkInfo();
+		if (netinfo.getDetailedState() == NetworkInfo.DetailedState.CONNECTED) {
+			viewThread.start();
+		} else {
+			Alert.showAlert(ViewBlog.this, "Network connection needed",
+					"Please, connect your device to the Internet");
+		}
+		viewProgress.setMessage("Viewing in progress...");
 	}
 
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
--- trunk/AndroBlogger/src/com/sadko/androblogger/ViewComments.java
+++ trunk/AndroBlogger/src/com/sadko/androblogger/ViewComments.java
@@ -1,5 +1,6 @@
 package com.sadko.androblogger;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,11 +9,23 @@ import java.util.Map;
 import com.google.gdata.data.Entry;
 import com.google.gdata.data.Feed;
 import com.google.gdata.data.TextContent;
+import com.google.gdata.util.ServiceException;
+import com.sadko.androblogger.db.DBAdapter;
+import com.sadko.androblogger.util.Alert;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -23,14 +36,55 @@ import android.widget.TextView;
 
 public class ViewComments extends ListActivity {
 	private final String TAG = "ViewComments";
+	private final String MSG_KEY = "value";
 	public static Entry currentEntry = null;
 	public static Feed resultCommentFeed = null;
+	private int attempt = 0;
+	private DBAdapter mDbHelper;
+	private static Cursor setting = null;
+	private static boolean viewOk = false;
+	private ProgressDialog viewProgress = null;
+	int viewStatus = 0;
+	
+	final Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			Bundle content = msg.getData();
+			String progressId = content.getString(MSG_KEY);
+			if (progressId != null) {
+				if (progressId.equals("1")) {
+					viewProgress.setMessage("Preparing blog config...");
+				} else if (progressId.equals("2")) {
+					viewProgress.setMessage("Authenticating...");
+				} else if (progressId.equals("3")) {
+					viewProgress.setMessage("Receiving post comments...");
+				} else if (progressId.equals("4")) {
+					viewProgress.setMessage("Done...");
+				}
+			}
+		}
+	};
+	
+	final Runnable mViewResults = new Runnable() {
+		public void run() {
+			showViewStatus();
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.viewcomments);
 
+		mDbHelper = new DBAdapter(this);
+		try {
+			mDbHelper.open();
+		} catch (SQLException e) {
+			Log.e(TAG, "Database has not opened");
+		}
+		setting = mDbHelper.fetchSettindById(1);
+		startManagingCursor(setting);
+		
 		currentEntry = ViewPost.currentEntry;
 		Log.i(TAG, "CurrentEntry obtained from ViewPost");
 
@@ -70,8 +124,17 @@ public class ViewComments extends ListActivity {
 
 		int w = this.getWindow().getWindowManager().getDefaultDisplay()
 				.getWidth() - 12;
-		((Button) this.findViewById(R.id.BackToViewPost)).setWidth(w);
+		((Button) this.findViewById(R.id.BackToViewPost)).setWidth(w/2);
+		((Button) this.findViewById(R.id.RefreshCommentsList)).setWidth(w/2);
 
+		this.findViewById(R.id.RefreshCommentsList).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						viewPostComments();
+					}
+				});
+		
 		this.findViewById(R.id.BackToViewPost).setOnClickListener(
 				new OnClickListener() {
 					@Override
@@ -83,6 +146,114 @@ public class ViewComments extends ListActivity {
 				});
 	}
 
+	protected void viewPostComments() {
+		viewProgress = ProgressDialog.show(ViewComments.this,
+				"Viewing post comments", "Starting to view post comments...");
+
+		Thread viewThread = new Thread() {
+			public void run() {
+				Bundle status = new Bundle();
+				mHandler.getLooper();
+				Looper.prepare();
+				Message statusMsg = mHandler.obtainMessage();
+				viewStatus = 0;
+				status.putString(MSG_KEY, "1");
+				statusMsg.setData(status);
+				mHandler.sendMessage(statusMsg);
+				String username = setting.getString(setting
+						.getColumnIndexOrThrow(DBAdapter.KEY_LOGIN));
+				String password = setting.getString(setting
+						.getColumnIndexOrThrow(DBAdapter.KEY_PASSWORD));
+				String postID = currentEntry.getId().split("post-")[1];
+				BlogInterface blogapi = null;
+				BlogConfigBLOGGER.BlogInterfaceType typeEnum = BlogConfigBLOGGER
+						.getInterfaceTypeByNumber(1);
+				blogapi = BlogInterfaceFactory.getInstance(typeEnum);
+				Log.d(TAG, "Using interface type: " + typeEnum);
+				blogapi.setInstanceConfig("");
+				status.putString(MSG_KEY, "2");
+				statusMsg = mHandler.obtainMessage();
+				statusMsg.setData(status);
+				mHandler.sendMessage(statusMsg);
+				String auth_id = null;
+				boolean authFlag = false;
+				attempt = 0;
+				while ((attempt <= MainActivity.AMOUNTOFATTEMPTS)
+						&& (!authFlag)) {
+					try {
+						auth_id = blogapi.getAuthId(username, password);
+						authFlag = true;
+						attempt = 0;
+					} catch (com.google.gdata.util.AuthenticationException e) {
+						Log.e(TAG, "AuthenticationException " + e.getMessage());
+						attempt++;
+					}
+				}
+				viewStatus = 1;
+				Log.d(TAG, "Got auth token:" + auth_id);
+				viewStatus = 2;
+				if (auth_id != null) {
+					status.putString(MSG_KEY, "3");
+					statusMsg = mHandler.obtainMessage();
+					statusMsg.setData(status);
+					mHandler.sendMessage(statusMsg);
+					attempt++;
+					authFlag = false;
+					while ((attempt <= MainActivity.AMOUNTOFATTEMPTS)
+							&& (!authFlag)) {
+						try {
+							ViewPost.resultCommentFeed = blogapi.getAllPostComments(
+									username, password, postID);
+							Log.i(TAG, "Post comments successfully received");
+							viewOk = true;
+							authFlag = true;
+							attempt = 0;
+						} catch (ServiceException e) {
+							e.printStackTrace();
+							attempt++;
+							Log.e(TAG, "ServiceException " + e.getMessage());
+						} catch (IOException e) {
+							e.printStackTrace();
+							attempt++;
+							Log.e(TAG, "IOException " + e.getMessage());
+						}
+					}
+
+				} else {
+					viewStatus = 3;
+				}
+				status.putString(MSG_KEY, "4");
+				statusMsg = mHandler.obtainMessage();
+				statusMsg.setData(status);
+				mHandler.sendMessage(statusMsg);
+				if (viewOk) {
+					Log.d(TAG, "Success!");
+					viewStatus = 5;
+				} else {
+					Log.d(TAG, "Viewing of comments failed!");
+					viewStatus = 4;
+				}
+				mHandler.post(mViewResults);
+
+				if ((resultCommentFeed != null) && (viewOk)) {
+					Intent i = new Intent(ViewComments.this, ViewComments.class);
+					startActivity(i);
+					finish();
+				}
+			}
+		};
+		ConnectivityManager cm = (ConnectivityManager) ViewComments.this
+				.getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo netinfo = cm.getActiveNetworkInfo();
+		if (netinfo.getDetailedState() == NetworkInfo.DetailedState.CONNECTED) {
+			viewThread.start();
+		} else {
+			Alert.showAlert(ViewComments.this, "Network connection needed",
+					"Please, connect your device to the Internet");
+		}
+		viewProgress.setMessage("Viewing in progress...");
+	}
+	
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
 			Intent i = new Intent(ViewComments.this, ViewPost.class);
@@ -92,5 +263,27 @@ public class ViewComments extends ListActivity {
 		}
 		return false;
 	}
-
+	
+	private void showViewStatus() {
+		viewProgress.dismiss();
+		if (attempt > MainActivity.AMOUNTOFATTEMPTS) {
+			/*
+			 * Alert.showAlert(this, "Viewing failed", "Error code " +
+			 * viewStatus); }
+			 */
+			Alert.showAlert(this, "Viewing failed", "Error code " + viewStatus,
+					"Try again", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+							viewPostComments();
+						}
+					}, "Cancel", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.cancel();
+						}
+					});
+		}
+	}
 }
